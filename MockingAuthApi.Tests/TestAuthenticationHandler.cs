63d445a21191f91633ab4e0505d6c560422122b9
Jack Wild <jackwild123@gmail.com>
2017-11-05 19:57:10 +0000

    Added fake authentication middleware.

---
+++ MockingAuthApi.Tests/TestAuthenticationHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+
+namespace MockingAuthApi.Tests
+{
+    public class TestAuthenticationHandler : AuthenticationHandler<TestAuthenticationOptions>
+    {
+        public TestAuthenticationHandler(IOptionsMonitor<TestAuthenticationOptions> options, ILoggerFactory logger,
+            UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var authenticationTicket = new AuthenticationTicket(
+                new ClaimsPrincipal(Options.Identity),
+                new AuthenticationProperties(),
+                "Test Scheme");
+
+            return Task.FromResult(AuthenticateResult.Success(authenticationTicket));
+        }
+    }
+
+    public static class TestAuthenticationExtensions
+    {
+        public static AuthenticationBuilder AddTestAuth(this AuthenticationBuilder builder, Action<TestAuthenticationOptions> configureOptions)
+        {
+            return builder.AddScheme<TestAuthenticationOptions, TestAuthenticationHandler>("Test Scheme", "Test Auth", configureOptions);
+        }
+    }
+
+    public class TestAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        public virtual ClaimsIdentity Identity { get; } = new ClaimsIdentity(new Claim[]
+        {
+            new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", Guid.NewGuid().ToString()),
+        }, "test");
+    }
+}
--- MockingAuthApi.Tests/When_Calling_PeopleController_Post.cs
+++ MockingAuthApi.Tests/When_Calling_PeopleController_Post.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.TestHost;
 using MockingAuthApi.Models;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,13 +12,13 @@ namespace MockingAuthApi.Tests
         [Fact]
         public async Task Then_Returns_BadRequest_When_ModelState_Invalid()
         {
-            var builder = new WebHostBuilder().UseStartup<Startup>();
+            var builder = new WebHostBuilder().UseStartup<TestStartup>();
             var testServer = new TestServer(builder);
             var client = testServer.CreateClient();
 
             var result = await client.PostAsJsonAsync("/api/people", new PersonModel());
 
-            Assert.IsType<BadRequestResult>(result);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
         }
     }
 }
